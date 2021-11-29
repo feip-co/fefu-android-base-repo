ba7a0848cfb7f0ab81b825331165fcdd33890073
rimmarque <rimmarque@gmail.com>
2021-11-29 21:35:38 +1000

    Add first task 2.0

--- app/src/main/java/ru/fefu/activitytracker/ActivityTracker.kt
+++ app/src/main/java/ru/fefu/activitytracker/ActivityTracker.kt
@@ -1,11 +1,32 @@
 package ru.fefu.activitytracker
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Button
+import android.content.Intent
+import android.widget.TextView
+
 
 class ActivityTracker : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tracker)
+
+        val button = findViewById<Button>(R.id.registry_button)
+        button.setOnClickListener(){
+            val intent = Intent(this, RegistryActivity::class.java)
+            startActivity(intent)
+        }
+
+        val button2 = findViewById<TextView>(R.id.login_button)
+        button2.setOnClickListener(){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
+
--- app/src/main/java/ru/fefu/activitytracker/LoginActivity.kt
+++ app/src/main/java/ru/fefu/activitytracker/LoginActivity.kt
@@ -4,16 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class RegistryActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registry)
+        setContentView(R.layout.activity_login)
     }
 
     fun back(view: View) {
         onBackPressed()
     }
 }
-
-
-
